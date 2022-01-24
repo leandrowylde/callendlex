835c76514e162cfabf26a26f11600ebabaec911a
Leandro Alves <leandro.job@live.com>
2022-01-23 22:13:15 -0300

    Config customer name

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,12 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
+# Customer name
+config :callendlex,
+  owner: %{
+    name: "Bigardone"
+  }
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
